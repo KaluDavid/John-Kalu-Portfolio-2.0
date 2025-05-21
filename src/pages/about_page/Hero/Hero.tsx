6533d3681843b42e4cc6145aa2cf3f898774e632
Kalu David <102557749+KaluDavid@users.noreply.github.com>
2025-05-21 08:04:26 +0100

    about page touch

--- src/pages/about_page/Hero/Hero.tsx
+++ src/pages/about_page/Hero/Hero.tsx
@@ -4,35 +4,34 @@ import { Message } from "../../../components/ui/Message";
 
 export function Hero() {
   return (
-    <div className="flex  pb-35 flex-col items-start justify-center gap-5 w-full">
-      <Roles>
-        <span>ABOUT ME</span>
-        <span className="rounded-full bg-gray-800 size-2.5"></span>
-      </Roles>
-
-      <div className="flex xl:flex-row flex-col   justify-between w-full  items-center xl:gap-0 gap-20">
-        <div className="flex items-start flex-col gap-5">
-          <H_Tags
-            text="I craft products that not only meet organizational objectives but also deliver exceptional user experiences."
-            style="sm:w-[90%]"
-          />
-          <Message>
-            <a
-              href="mailto:Mrkalujohn@gmail.com"
-              className="flex items-center gap-2"
-            >
-              View my Resume
-            </a>
-          </Message>
-        </div>
-        <fieldset className="flex xl:flex-row flex-col   justify-between w-full  items-center">
-          <img
-            src="/images/profile.png"
-            alt="john kalu"
-            className="xl:w-[464px]"
-          />
-        </fieldset>
+    <div className="flex xl:flex-row flex-col pb-35  justify-between w-full  items-start xl:gap-0 gap-20">
+      <div className="flex items-start flex-col gap-5">
+        <Roles>
+          <span>ABOUT ME</span>
+          <span className="rounded-full bg-gray-800 size-2.5"></span>
+        </Roles>
+        <H_Tags style="sm:w-[90%]">
+          <span>
+            I craft products that not only meet organizational objectives but
+            also <br /> deliver exceptional user experiences."
+          </span>
+        </H_Tags>
+        <Message>
+          <a
+            href="mailto:Mrkalujohn@gmail.com"
+            className="flex items-center gap-2"
+          >
+            View my Resume
+          </a>
+        </Message>
       </div>
+      <fieldset className="flex xl:flex-row flex-col   justify-between w-full  items-center">
+        <img
+          src="/images/profile.png"
+          alt="john kalu"
+          className="xl:min-w-[464px]"
+        />
+      </fieldset>
     </div>
   );
 }
